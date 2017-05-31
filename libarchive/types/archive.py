b8fc41bf878361fac78f63e0dda66706523efea5
Forness <jforness@Jody-Forness-Macbook-Pro2.local>
2017-05-31 14:42:43 -0700

    Fix for issue #5, ctypes import error in Python 2.6.6

--- libarchive/types/archive.py
+++ libarchive/types/archive.py
@@ -1,5 +1,10 @@
 from ctypes import *
 
+try:
+    from ctypes import c_ssize_t
+except ImportError:
+    from ctypes import c_longlong as c_ssize_t
+
 ARCHIVE_WRITE_CALLBACK = CFUNCTYPE(c_ssize_t, c_void_p, c_void_p, POINTER(c_void_p), c_size_t)
 ARCHIVE_OPEN_CALLBACK = CFUNCTYPE(c_int, c_void_p, c_void_p)
 ARCHIVE_CLOSE_CALLBACK = CFUNCTYPE(c_int, c_void_p, c_void_p)
